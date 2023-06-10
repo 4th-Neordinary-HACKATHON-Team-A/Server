210af63846937b137690060c11fafd8daad492fb
choidongkuen <danaver12@daum.net>
2023-06-11 04:03:40 +0900

    [#39] FEAT : ErrorCode 상수 추가

--- src/main/java/com/rememory/exception/ErrorCode.java
+++ src/main/java/com/rememory/exception/ErrorCode.java
@@ -8,8 +8,10 @@ import lombok.Getter;
 public enum ErrorCode {
 
     USER_NOT_FOUND("해당하는 회원이 존재하지 않습니다."),
+    USER_ALREADY_EXIST("해당하는 회원이 이미 존재합니다."),
     IMAGE_NOT_FOUND("해당하는 이미지가 존재하지 않습니다."),
     POST_NOT_FOUND("해당하는 게시글이 존재하지 않습니다."),
+    CATEGORY_NOT_FOUND("해당하는 카테고리가 존재하지 않습니다."),
     INTERNAL_SERVER_ERROR("서버 오류가 발생했습니다.");
 
     private final String errorMessage;
