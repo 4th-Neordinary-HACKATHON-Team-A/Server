71dd5591356063af85ab4165541dc7c0136b20d8
choidongkuen <danaver12@daum.net>
2023-06-11 00:24:48 +0900

    [#7] FEAT : DalleService 구현 및 관련 DTO 구현

---
+++ src/main/java/com/rememory/dto/DalleDto.java
@@ -0,0 +1,29 @@
+package com.rememory.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+public class DalleDto {
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class DalleRequestDto {
+        @NotBlank
+        private String prompt;
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class DalleResponseDto {
+        private String url;
+    }
+}
---
+++ src/main/java/com/rememory/service/dalle/DalleService.java
@@ -0,0 +1,29 @@
+package com.rememory.service.dalle;
+
+import com.rememory.dto.DalleDto.DalleRequestDto;
+import com.theokanning.openai.image.CreateImageRequest;
+import com.theokanning.openai.image.ImageResult;
+import com.theokanning.openai.service.OpenAiService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+public class DalleService {
+
+    @Value("${spring.api-key}")
+    private String apiKey;
+
+    public ImageResult getDalleResponse(DalleRequestDto request) throws Exception {
+        Duration duration = Duration.ofMinutes(5);
+        OpenAiService service = new OpenAiService(apiKey, duration);
+
+        CreateImageRequest createImageRequest
+                = CreateImageRequest.builder()
+                                    .prompt(request.getPrompt()).build();
+
+        return service.createImage(createImageRequest);
+
+    }
+}
