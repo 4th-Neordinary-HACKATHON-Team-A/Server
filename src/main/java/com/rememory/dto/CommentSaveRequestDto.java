f7ae04c48eced5b626242bb9e0e3be9c646f4628
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2023-06-11 03:20:39 +0900

    [FEAT] 엔티티 컬럼 valid 설정 및 service 수정 (#48)
    [FEAT] 엔티티 컬럼 valid 설정 및 service 수정

--- src/main/java/com/rememory/dto/CommentSaveRequestDto.java
+++ src/main/java/com/rememory/dto/CommentSaveRequestDto.java
@@ -2,7 +2,10 @@ package com.rememory.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Getter
 @Builder
@@ -10,6 +13,8 @@ import javax.validation.constraints.NotBlank;
 @NoArgsConstructor
 public class CommentSaveRequestDto {
     @NotBlank(message = "내용을 작성해주세요")
+    @Min(1)
+    @Max(200)
     private String text;
 
     @NotBlank(message = "작성자를 작성해주세요.")
--- src/main/java/com/rememory/dto/UserSaveRequestDto.java
+++ src/main/java/com/rememory/dto/UserSaveRequestDto.java
@@ -4,6 +4,8 @@ import com.rememory.domain.user.User;
 import lombok.*;
 import org.hibernate.validator.constraints.UniqueElements;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
 @Getter
@@ -13,6 +15,8 @@ import javax.validation.constraints.NotBlank;
 public class UserSaveRequestDto {
     @NotBlank(message = "닉네임을 작성해주세요")
     @UniqueElements(message = "중복된 닉네임입니다")
+    @Min(1)
+    @Max(255)
     private String nickname;
 
     public User toEntity() {
