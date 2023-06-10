e04816192836bc301c02ab9faea3dfc569bb4b51
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2023-06-11 02:58:19 +0900

    [REFACTOR] 게시글 관련 Controller, Service 수정 (#45)
    게시글 관련 Controller, Service 수정

--- src/main/java/com/rememory/controller/PostApiController.java
+++ src/main/java/com/rememory/controller/PostApiController.java
@@ -1,4 +1,11 @@
 package com.rememory.controller;
+import com.rememory.dto.CommentSaveRequestDto;
+import com.rememory.dto.PostSaveRequestDto;
+import com.rememory.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
 
 @RequiredArgsConstructor
@@ -8,8 +15,23 @@ public class PostApiController {
     private final PostService postService;
 
     @PostMapping
-    public ResponseEntity<Void> create(@RequestBody @Valid PostSaveRequestDto requestDto){
-        postService.save(requestDto);
+    public ResponseEntity<Void> createPost(@RequestBody @Valid PostSaveRequestDto requestDto){
+        postService.savePost(requestDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/{postId}/{userId}/like")
+    public ResponseEntity<Void> registerLike(
+            @PathVariable(name = "postId") Long postId,
+            @PathVariable(name = "userId") Long userId
+    ) {
+        this.postService.registerLike(postId, userId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/comment")
+    public ResponseEntity<Void> createComment(@RequestBody @Valid CommentSaveRequestDto requestDto){
+        postService.saveComment(requestDto);
         return ResponseEntity.ok().build();
     }
 }
