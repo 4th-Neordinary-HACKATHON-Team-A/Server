630355f0f97352cb87fcebe5fc404857db0801c7
choidongkuen <danaver12@daum.net>
2023-06-10 18:51:51 +0900

    [#14] FEAT : Like 도메인 구현 완료

---
+++ src/main/java/com/rememory/domain/like/Like.java
@@ -0,0 +1,32 @@
+package com.rememory.domain.like;
+
+import com.rememory.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+
+@Slf4j
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+
+    @Column(nullable = false)
+    private boolean isLiked;
+}
