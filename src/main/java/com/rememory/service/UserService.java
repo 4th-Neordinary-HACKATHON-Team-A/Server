82a9fe981d23ca36b3f9583a56ea9f127277735b
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2023-06-10 23:42:39 +0900

    [FEAT] 회원 가입 API 구현 (#31)
    * [FEAT] 회원 가입 API 구현
    
    * [REFACTOR] 사용하지 않는 테스트 코드 주석처리
    
    * [REFACTOR] 닉네임 중복 체크
    
    * [TEST] UserRepositoryTest 작성

---
+++ src/main/java/com/rememory/service/UserService.java
@@ -0,0 +1,19 @@
+package com.rememory.service;
+
+import com.rememory.domain.user.UserRepository;
+import com.rememory.dto.UserSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Transactional
+    public Long save(UserSaveRequestDto requestDto){
+        return userRepository.save(requestDto.toEntity()).getId();
+    }
+}
