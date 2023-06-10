5cd5dd0ac47058e7fca1bbd2e5393d5be4d37c0b
choidongkuen <danaver12@daum.net>
2023-06-10 18:32:37 +0900

    [#15] FEAT : image 엔티티 및 레포지토리를 구현

---
+++ src/main/java/com/rememory/domain/image/Image.java
@@ -0,0 +1,26 @@
+package com.rememory.domain.image;
+
+import com.rememory.domain.user.User;
+import com.rememory.global.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Image extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Column(nullable = false)
+    private String imageSrc;
+}
