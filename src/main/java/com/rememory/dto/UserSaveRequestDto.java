3b38b3a3f13e9b6c63ef8a1e7db3b5b84f04d1a4
SunKyoung Yoon <yskkjdh@gmail.com>
2023-06-10 23:41:28 +0900

    [REFACTOR] 닉네임 중복 체크

--- src/main/java/com/rememory/dto/UserSaveRequestDto.java
+++ src/main/java/com/rememory/dto/UserSaveRequestDto.java
@@ -2,6 +2,7 @@ package com.rememory.dto;
 
 import com.rememory.domain.user.User;
 import lombok.*;
+import org.hibernate.validator.constraints.UniqueElements;
 
 import javax.validation.constraints.NotBlank;
 
@@ -12,6 +13,7 @@ import javax.validation.constraints.NotBlank;
 @NoArgsConstructor
 public class UserSaveRequestDto {
     @NotBlank(message = "닉네임을 작성해주세요")
+    @UniqueElements(message = "중복된 닉네임입니다")
     private String nickname;
 
     public User toEntity() {
