ecb5d58d0ac9b7a0f287f4d5336c1520c3c10089
ChoiDongKuen <danaver12@daum.net>
2023-06-11 00:27:36 +0900

    [FEAT] 이미지 생성 API를 생성한다 (#33)
    * [#7] FEAT : application.properties 설정
    
    * [#7] FIX : Like 예약어 오류 수정 -> Likes 로 수정
    
    * [#7] FEAT : DalleService 구현 및 관련 DTO 구현

--- src/main/java/com/rememory/dto/DalleDto.java
+++ src/main/java/com/rememory/dto/DalleDto.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
-import java.util.ArrayList;
 
 public class DalleDto {
 
@@ -16,15 +15,8 @@ public class DalleDto {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class DalleRequestDto {
-
         @NotBlank
         private String prompt;
-
-        private int n = 1;
-
-        @NotBlank
-        private String size;
-
     }
 
     @Getter
@@ -32,16 +24,7 @@ public class DalleDto {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class DalleResponseDto {
-
-        private Long created;
-        private ArrayList<UrlResponseDto> data;
-    }
-
-    @Getter
-    @Builder
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public static class UrlResponseDto {
         private String url;
     }
 }
+
