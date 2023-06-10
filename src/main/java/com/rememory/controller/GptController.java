d4e477ff9766624e31a19d661659a59d97f59872
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2023-06-11 01:38:37 +0900

    [REFACTOR] 문장 요약 및 이미지 생성 기능 병합(#36) (#42)

--- src/main/java/com/rememory/controller/GptController.java
+++ src/main/java/com/rememory/controller/GptController.java
@@ -1,8 +1,11 @@
 package com.rememory.controller;
 
 
+import com.rememory.dto.GptDto.*;
+import com.rememory.service.dalle.DalleService;
 import com.rememory.service.gpt.GptService;
 import com.rememory.service.gpt.vo.GptReqVo;
+import com.theokanning.openai.completion.CompletionChoice;
 import com.theokanning.openai.completion.CompletionResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +21,8 @@ public class GptController {
 
     @Autowired
     private GptService gptService;
+    @Autowired
+    private DalleService dalleService;
 
     @PostMapping("/summary")
     public CompletableFuture<CompletionResult> summaryToGpt(@RequestBody GptReqVo gptReqVo) throws Exception {
@@ -32,15 +37,23 @@ public class GptController {
         return result;
     }
 
-    @PostMapping("/prompt")
-    public CompletableFuture<CompletionResult> promptToGpt(@RequestBody GptReqVo gptReqVo) throws Exception {
-        String strQuestion =  "\"" + gptReqVo.getSentence() + "\"" +
-                "\n\n 위 문장을 영어로 번역해줘.";
+    @PostMapping("")
+    public GptImageResponseDto requestToGpt(@RequestBody GptReqVo gptReqVo) throws Exception {
+        String strQuestion = "\"" + gptReqVo.getSentence() + "\"" +
+                "\n\n 위 문장을 200자 내외로 요약해줘.";
         System.out.println("프롬프트 확인: " + strQuestion);
 
-        CompletableFuture<CompletionResult> result =
+        CompletableFuture<CompletionResult> summaryRespons =
                 gptService.getGptResponse(strQuestion);
-        System.out.println("Generate an image of \"" + result.get().getChoices().get(0).getText().replaceAll("\n", "") + "\"");
+        CompletionChoice choice = summaryRespons.get().getChoices().get(0);
+        String summary = choice.getText().replaceAll("\n", "");
+        System.out.println(summary);
+
+        String url = String.valueOf(gptService.promptToGpt(summary).getData().get(0));
+        GptImageResponseDto result = GptImageResponseDto.builder()
+                .choice(choice)
+                .imageUrl(url)
+                .build();
 
         return result;
     }
