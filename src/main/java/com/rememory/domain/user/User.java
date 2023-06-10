3c5abb4543bdbfd54e12d8d8eb45065b2cf58102
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2023-06-10 18:07:36 +0900

    [#20] FEAT : User 도메인을 설계한다
    * FEAT: BaseTimeEntity 도메인 설계
    
    * FEAT: User 도메인 설계
    
    * REFACTOR: User 도메인 설계 수정

---
+++ src/main/java/com/rememory/domain/user/User.java
@@ -0,0 +1,28 @@
+package com.rememory.domain.user;
+
+import com.rememory.global.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String nickname;
+}
+
